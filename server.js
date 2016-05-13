db7a61d947422892f139283e8101e35f931e8620
Sachin Mour <rtr.sachinmour@gmail.com>
2016-05-13 21:28:01 +0000

    Initialize repository

---
+++ server.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var routes = require('./app/routes/index.js');
+var mongoose = require('mongoose');
+var passport = require('passport');
+var session = require('express-session');
+var expressValidator = require('express-validator');
+var bodyParser = require('body-parser');
+var flash = require('express-flash');
+
+var app = express();
+require('dotenv').load();
+require('./app/config/passport')(passport);
+
+mongoose.connect(process.env.MONGO_URI);
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(expressValidator());
+app.use(flash());
+
+app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
+app.use('/public', express.static(process.cwd() + '/public'));
+app.use('/common', express.static(process.cwd() + '/app/common'));
+
+app.use(session({
+	secret: 'secretPoll',
+	resave: false,
+	saveUninitialized: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+routes(app, passport);
+
+var port = process.env.PORT || 8080;
+app.listen(port,  function () {
+	console.log('Node.js listening on port ' + port + '...');
+});
